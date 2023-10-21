bc64570850bd045b14dcacb0258536436804869e
mason <mason@toss.im>
2023-10-21 14:05:48 +0900

    add git plugins

--- lua/plugins/own-config.lua
+++ lua/plugins/own-config.lua
@@ -11,4 +11,14 @@ return {
       },
     },
   },
+  {
+    "kdheepak/lazygit.nvim",
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+  {
+    "tpope/vim-fugitive",
+  },
 }
