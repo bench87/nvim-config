1f1598681ef65d556010d01633442b52f2cc1850
mason <mason@toss.im>
2023-10-21 13:47:58 +0900

    add persistence undo

--- init.lua
+++ init.lua
@@ -1,2 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+local undodir = vim.fn.stdpath("config") .. "/undodir"
+
+if vim.fn.isdirectory(undodir) == 0 then
+  vim.fn.mkdir(undodir, "p")
+end
+
+-- Set undodir and enable undofile
+vim.opt.undodir = undodir
+vim.opt.undofile = true
