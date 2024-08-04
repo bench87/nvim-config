7fafeb91e68b7215734c5ae862adefaf56fe467f
mason <bench8704@gmail.com>
2024-08-04 17:27:24 +0900

    fix ignore patterns

--- lua/plugins/telscope.lua
+++ lua/plugins/telscope.lua
@@ -2,7 +2,7 @@ return {
   "nvim-telescope/telescope.nvim",
   opts = {
     defaults = {
-      file_ignore_patterns = { "out/**" },
+      file_ignore_patterns = { "out/", ".metals/", ".git", ".bloop/" },
     },
   },
 }
